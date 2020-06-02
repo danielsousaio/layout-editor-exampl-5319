6f14af44f9aae871a5260f90c16da52b60342212
Daniel Sousa <mail@danielsousa.io>
2020-06-02 11:06:34 +0100

    Update blank.js JSX

--- src/features/BlankScreen4955/screens/blank.js
+++ src/features/BlankScreen4955/screens/blank.js
@@ -1,19 +1,28 @@
-import React from "react";
-import { View } from "react-native";
-import { withStyles, Text, Button, CheckBox, Radio, Toggle, Icon, Input } from 'react-native-ui-kitten';
+import React, { Component } from "react"
+import { View } from "react-native"
+import { Text, Layout, Button } from "react-native-ui-kitten"
+import { connect } from "react-redux"
+import { GoogleSignin, GoogleSigninButton } from "react-native-google-signin"
+import { styles } from "./styles"
+import * as calendarActions from "../../redux/actions"
+import ErrorBox from "../../../../components/ErrorBox"
 
 export class _Blank extends React.Component {
   static navigationOptions = {
     title: "Blank".toUpperCase()
-  };
+  }
 
-  state = { }
+  state = {}
 
-  render = () => <View style={this.props.themedStyle.container}></View>;
+  render = () => (
+    <CbFlex title="Button123" color="#FF8040" styles={{ overflow: "visible" }}>
+      <CbFlex styles={{ overflow: "visible" }} />
+    </CbFlex>
+  )
 }
 
 export default Blank = withStyles(_Blank, theme => ({
   container: {
     backgroundColor: theme["color-basic-100"]
   }
-}));
+}))
